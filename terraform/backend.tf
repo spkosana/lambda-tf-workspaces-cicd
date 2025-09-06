985790ca8c6ec7ab4bda8a3feb7cbcbd6a4e4a11
Surya Kosana <spkosana.82@gmail.com>
2025-09-06 12:39:27 -0400

    Adding terraform and github workflows

---
+++ terraform/backend.tf
@@ -0,0 +1,29 @@
+terraform {
+
+  # Comment this out when initialising resources.
+  backend "s3" {
+    region         = "us-east-2"
+    bucket         = "aws-glue-terraform-state-kspr"
+    key            = "lambda-tf-workspaces-cicd/workspaces/terraform.tfstate"
+    dynamodb_table = "eks-terraform"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.11.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = {
+      Environment = "${terraform.workspace}"
+      Project     = "LAMBDA-TF-WORKSPACES-CICD"
+      Owner       = "Surya Kosana"
+    }
+  }
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-2"
+  type        = string
+}
